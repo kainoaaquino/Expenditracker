5eb60a016e33a9d1ca8ff16a711e03f0a77c893f
Kainoa Aquino <kainoa104@gmail.com>
2022-09-15 19:26:15 -0700

    switched to cloud

--- src/main/java/util/connectionUtil.java
+++ src/main/java/util/connectionUtil.java
@@ -11,8 +11,8 @@ public class connectionUtil {
         if(conn == null) {
             try
             {
-                String url = "jdbc:sqlserver://localhost:1433;TrustServerCertificate=True";
-                String username = "sa";
+                String url = "jdbc:sqlserver://kainacho.database.windows.net:1433;database=db1;user=kainacho@kainacho;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+                String username = "kainacho";
                 String password = "P@SSWORD123";
                 conn = DriverManager.getConnection(url, username, password);
             } catch(SQLException e)
